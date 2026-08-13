c89df3379b96bbe3a9eb7780f58f1cb1856a0411
theangelical777-creator <theangelical777-creator@users.noreply.github.com>
2026-08-13 02:43:31 -0400

    Prepare GitHub Pages deployment and secure API config

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using ProdigioApp;
 using ProdigioApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+builder.Configuration.AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: false);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
